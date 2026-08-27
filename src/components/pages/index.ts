e4230bcf929537326fdf3f330fcdc26adcaa3edd
steveasterafovo <steveasterafovo@gmail.com>
2026-08-27 23:29:59 +0100

    s

---
+++ src/components/pages/index.ts
@@ -0,0 +1,4 @@
+export { HeroSection } from "./HeroSection";
+export { DemoRoomsSection } from "./DemoRoomsSection";
+export { PillarsSection } from "./PillarsSection";
+export { MeetPreviewIllustration } from "./MeetPreviewIllustration";
