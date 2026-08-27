d2e7e3a1186af1b1fd10672c09aaaf69f41b0b4c
steveasterafovo <steveasterafovo@gmail.com>
2026-08-28 00:10:49 +0100

    s

--- src/routes/privacy.tsx
+++ src/routes/privacy.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { SiteFooter, SpcLogo } from "@/components/site/Brand";
+import { PageFooter } from "@/components/site";
 
 export const Route = createFileRoute("/privacy")({
   head: () => ({
