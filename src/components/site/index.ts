782f431c81dd007e8596e1027fd1dfb369f40a42
steveasterafovo <steveasterafovo@gmail.com>
2026-09-13 15:28:20 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,3 +1,5 @@
 export { PageHeader } from "./PageHeader";
 export { PageFooter } from "./PageFooter";
 export { MeetShell } from "./MeetShell";
+export { ThemeToggle } from "./ThemeToggle";
+export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
